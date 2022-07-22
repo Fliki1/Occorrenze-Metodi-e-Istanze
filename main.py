1d6021e7f4aac865405d66fef2e794fc137357b8
Flik1 <leo.pelonero@hotmail.it>
2022-07-22 12:38:20 +0200

    :recycle: Main refactoring

--- main.py
+++ main.py
@@ -1,34 +1,7 @@
 from pydriller import Repository
 import pandas as pd
-import re
 
-from src import Parse
-
-
-"""def parseLine(line):
-    lineTokens = list(javalang.tokenizer.tokenize(line))
-    values = []
-    types = []
-    for token in lineTokens:
-        values.append(token.value)
-        types.append(type(token).__name__)
-    lineList = list(zip(values, types))
-    return parseIdentifier(lineList)
-
-
-def parseIdentifier(lineList):
-    auxList = lineList.copy()
-    for i in range(len(auxList)):
-        if auxList[i][1] == 'Identifier':
-            if auxList[i + 1][0] == '(' and auxList[i - 1][0] == '.':
-                lineList[i] = (lineList[i][0], 'Method')
-            elif auxList[i + 1][0] == '(':
-                lineList[i] = (lineList[i][0], 'Function')
-            elif auxList[i][0][0].isupper():
-                lineList[i] = (lineList[i][0], 'Class')
-            else:
-                lineList[i] = (lineList[i][0], 'Variable')
-    return lineList"""
+from src import Parse, Comment
 
 
 def addVariables(table, tokens, filename):
@@ -76,76 +49,92 @@ def checkVariableClass(table, filename, varname):
     auxset = table[table["Filename"] == filename]
     auxset = auxset[auxset["Varname"] == varname]
     if auxset.shape[0] > 1:
-        print("Multiple variables with the same name detected in the same file.")
+        #print("Multiple variables with the same name detected in the same file.")
         return
     elif auxset.shape[0] == 0:
-        print("Variable declaration not previously found")
+        #print("Variable declaration not previously found")
         return
     else:
         return auxset.iat[0, 2]
 
 
-def isComment(reList, string):
-    matches = []
-    for regEx in reList:
-        matches.append(regEx.search(string))
-    if matches.count(None) != len(matches):
-        return True
-    return False
-
-
 def lookForClasses(file):
+    """
+    Dato l'intero codice di un file si cerca la presenza di tutte le classi e corrispondente riga
+    :param file: intero codice di un file
+    :return: lista contenenti (riga, classe) presenti nel singolo file
+    """
     if file == None:
         return
     classes = []
-    lines = file.split("\n")
+    lines = file.split("\n")        # split per \n l'intero codice
+    # print(lines)                  # ['', '// import java.util.*;  ', 'import java.util.Scanner; // Import the Scanner class', '', 'class Main {',
     for i in range(len(lines)):
-        tokens = lines[i].split(" ")
+        tokens = lines[i].split(" ")                # split per " " codice già splittato da \n
+        # print("tokens", tokens)                   # [''] - ['//', 'import', 'java.util.*;', '', ''] - [
         if "class" in tokens:
-            classes.append((i, tokens[tokens.index("class") + 1]))
+            if tokens.index("class") + 1 < len(tokens):     # se esiste un riferimento dopo 'class'
+                classes.append((i, tokens[tokens.index("class") + 1]))
+    # print(classes) [(1, 'ConstructorOverloading'), (20, 'House')] [(4, 'Main')] [(4, 'Main')] []
     return classes
 
 
 def getActiveClass(classes, line_num):
+    """
+    Riconoscere la classe analizzata based on lookForClasses precedentemente eseguito (riga, classe)
+    :param classes: lista di classi a conoscenza [(1, 'test'), (22, 'ParentTest'), (33, 'test')]
+    :param line_num: riga analizzata
+    :return: Classe associata a quella riga secondo lookFroClasses eseguito in precedenza, None se non presente
+    """
     activeClass = None
+    #print("classes", classes)
+    #print("line_num", line_num)
     for aux in classes:
         if line_num >= aux[0]:
             activeClass = aux[1]
     return activeClass
 
-
-dataset = pd.DataFrame(columns=["Filename", "Change type", "Line number", "Code", "Tokens", "NumEdit"], \
-                       index=[])
+"""
+    DataFrame di supporto per la gestione e analisi dei dati:
+        dataset = tutte le linee di codice
+        variables = associa ogni istanza alle sue classi di appartenenza
+        methods = associa ogni metodo alla sua classe (in relazione all'istanza di appartenenza)
+"""
+dataset = pd.DataFrame(columns=["Filename", "Change type", "Line number", "Code", "Tokens", "NumEdit"], index=[])
 variables = pd.DataFrame(columns=["Filename", "Varname", "Vartype"], index=[])
 methods = pd.DataFrame(columns=["Filename", "MethodName", "Class", "CallingClass", "Line number"], index=[])
 
-reMethodCall = re.compile('\s*\S+\s*\.\s*\S+\s*\(.*\).*;\s*')
-reInstAss = re.compile('\s*\S+\s*=\s*new\s*\S+\s*;\s*')
-
-reIsComment = []
-reIsComment.append(re.compile('\s*//.*'))
-reIsComment.append(re.compile('\s*/\*.*'))
-
-# reClassDeclaration = re.compile(".*class\s+\S+\s*")
-
-# for commit in Repository('https://github.com/mauricioaniche/repodriller').traverse_commits():
-for commit in Repository('https://github.com/tdebatty/java-LSH').traverse_commits():
+for commit in Repository('https://github.com/niharika2k00/Java').traverse_commits():
+    #per ogni commit
     for file in commit.modified_files:
+        # se è stato modificato un file .java
         if (file.change_type.name == "ADD" or file.change_type.name == "MODIFY" \
             or file.change_type.name == "DELETE") and file.filename[-5:] == ".java":
+
+            # nome classe
             name = file.filename
+            change = file.change_type.name  # ADD - MODIFY ...
+
+            # ogni riga aggiunta o cancellata nel file {'added': [(1, ''), (2, '// import java.util.*;'), (3,... 'delete': ...}
             diff = file.diff_parsed
-            change = file.change_type.name
             added = diff["added"]
             deleted = diff["deleted"]
-            lines = deleted + added
+            lines = deleted + added     # [(1, ''), (2, '// import java.util.*;'), (3,... puro codice solo modificato nel commit
             cutOffPoint = len(added)
+
+            # print(file.source_code)   # file.source_code = source code of the file (can be _None_ if the file is deleted or only renamed)
+            # print(file.source_code_before)   # file.source_code_before = source code of the file before the change (can be _None_ if the file is added or only renamed)
+
+            # lista (riga, classe) del codice corrente
             classesInAdded = lookForClasses(file.source_code)
-            classesInDeleted = lookForClasses(file.source_code_before)
+            # lista (riga, classe) del codice come era prima
+            classesInDeleted = lookForClasses(file.source_code_before)  # perché?
+
             for i, element in zip(range(len(lines)), lines):
-                matchMethodCall = reMethodCall.search(element[1])
-                matchInstAss = reInstAss.search(element[1])
-                if isComment(reIsComment, element[1]):
+                print("deleted + added ", i , element)
+                matchMethodCall = Comment.reMethodCall.search(element[1])
+                matchInstAss = Comment.reInstAss.search(element[1])
+                if Comment.isComment(Comment.reIsComment, element[1]):
                     continue
                 if matchMethodCall or matchInstAss:
                     tokens = Parse.parseLine(element[1])
@@ -154,7 +143,8 @@ for commit in Repository('https://github.com/tdebatty/java-LSH').traverse_commit
                     else:
                         ind = dataset.loc[(dataset["Filename"] == name) & (dataset["Line number"] == element[0])].index
                         if len(ind) != 1:
-                            print("counted the wrong number of indices")
+                            print("")
+                            #print("counted the wrong number of indices")
                         ind = ind[0]
                         dataset.at[ind, "Change type"].append(change)
                         dataset.at[ind, "Code"].append(element[1])
