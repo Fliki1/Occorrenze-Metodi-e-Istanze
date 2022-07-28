06247197b2807d49abdd6578dfc8d1f924056700
Flik1 <leo.pelonero@hotmail.it>
2022-07-28 23:56:07 +0200

    :recycle: Print

--- src/Print.py
+++ src/Print.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import os
 
+
 def path(repo_name):
-    if not os.path.exists("./results/"+repo_name):
-        os.mkdir("./results/"+repo_name)
+    if not os.path.exists("./results/" + repo_name):
+        os.mkdir("./results/" + repo_name)
+
 
 def printData(repo_name, dataset, variables, methods):
-
     path(repo_name)
     # Fare il set_index dei dataframe causa problemi con la successiva analisi e gioco dei dati...
     # evitare se bisogna utilizzare questi per un analisi o conteggio vedi: methods
@@ -14,24 +15,25 @@ def printData(repo_name, dataset, variables, methods):
     # Printing Dataset
     dataset.set_index(["Filename", "Line number"], inplace=True)
     dataset.sort_index(inplace=True)
-    dataset.to_csv("./results/"+repo_name+"/DataSet-commitTable.csv")
-    dataset.to_excel("./results/"+repo_name+"/DataSet-commitTable.xlsx")
+    dataset.to_csv("./results/" + repo_name + "/DataSet-commitTable.csv")
+    dataset.to_excel("./results/" + repo_name + "/DataSet-commitTable.xlsx")
     # print(dataset[:20])
 
     # Printing Variables
     variables.set_index(["Filename", "Varname"], inplace=True)
     variables.sort_index(inplace=True)
-    variables.to_csv("./results/"+repo_name+"/VariablesTable.csv")
-    variables.to_excel("./results/"+repo_name+"/VariablesTable.xlsx")
+    variables.to_csv("./results/" + repo_name + "/VariablesTable.csv")
+    variables.to_excel("./results/" + repo_name + "/VariablesTable.xlsx")
     # print(variables[:20])
 
     # Printing Methods
-    methods.to_csv("./results/"+repo_name+"/MethodsTable.csv")
-    methods.to_excel("./results/"+repo_name+"/MethodsTable.xlsx")
+    methods.to_csv("./results/" + repo_name + "/MethodsTable.csv")
+    methods.to_excel("./results/" + repo_name + "/MethodsTable.xlsx")
     # print(methods_print[:20])
 
     # Printing Methods
-    method_count = methods.value_counts(["MethodName", "Class"])  # Return a Series containing counts of unique rows in the DataFrame.
+    method_count = methods.value_counts(
+        ["MethodName", "Class"])  # Return a Series containing counts of unique rows in the DataFrame.
     # method_count.columns = ["MethodName", "Class", "Count"]       # Rinomina le colonne
     method_count.rename("Count", inplace=True)
 
@@ -50,6 +52,5 @@ def printData(repo_name, dataset, variables, methods):
     method_count = pd.concat([method_count, classes], axis=1)
     method_count.reset_index(inplace=True)
     # Save
-    method_count.to_csv("./results/"+repo_name+"/FinalMethodTable.csv", index=False)
-    method_count.to_excel("./results/"+repo_name+"/FinalMethodTable.xlsx", index=False)
-
+    method_count.to_csv("./results/" + repo_name + "/FinalMethodTable.csv", index=False)
+    method_count.to_excel("./results/" + repo_name + "/FinalMethodTable.xlsx", index=False)
